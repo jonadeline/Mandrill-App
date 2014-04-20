8d3131169b5dbb62ad1f248e7c39d0312b9ae749
Bodhiz <jonathan.adeline@gmail.com>
2014-04-20 17:15:41 +0200

    Lot of changes...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,20 @@
+/*global mandrill:false */
 'use strict';
-
 angular.module('mandrillApp', ['ngRoute', 'LocalStorageModule'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/send-ecard.html',
-        controller: 'sendCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'partials/send-ecard.html',
+                controller: 'sendCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .factory('mandrill', function() {
+        return{
+          initMandrill: function(){
+            return new mandrill.Mandrill('ReWf5qM6RFFZH-KMqCz4jA', true);
+          }
+        };
+    });
--- app/scripts/controllers/sendCtrl.js
+++ app/scripts/controllers/sendCtrl.js
@@ -1,8 +1,12 @@
+/*global $:false */
 'use strict';
 
 angular.module('mandrillApp')
-    .controller('sendCtrl', function($scope, localStorageService, myServices) {
+    .controller('sendCtrl', function($scope, localStorageService, mandrill) {
 
+        //Init mandrill
+        var m = mandrill.initMandrill();
+        var params = { key : m.apikey };
 
         //useful regexp for form verification
         $scope.pattern1 = /^[A-Za-z\s]+$/; // Letters, spaces
@@ -10,172 +14,116 @@ angular.module('mandrillApp')
 
         //initialize recipients array
         $scope.recipients = [];
+        $scope.emailTemplates = [];
 
-        //Initialize Mandrill with API KEY
-        var m = new mandrill.Mandrill('7df1de52-51f9-4671-927d-bd431d352b46', true);
-
-        //Pre-populate form with user data
+        //Populate form with user data if available
         if (localStorageService.isSupported) {
-            if (localStorageService.get('userData'))
+            if (localStorageService.get('userData')) {
                 $scope.user = localStorageService.get('userData');
-        } else {
-            localStorageService.cookie.get('userData', $scope.user);
+            } else {
+                localStorageService.cookie.get('userData', $scope.user);
+            }
         }
 
-        // Store user's filled data with LocalStorage if supported, if not store in cookie
+        // Store user's data in localstorage or cookie.
         $scope.$watch(function() {
-            if (localStorageService.isSupported)
+            if (localStorageService.isSupported) {
                 localStorageService.add('userData', $scope.user);
-            else
+            } else {
                 localStorageService.cookie.add('userData', $scope.user);
+            }
         });
-
 
         $scope.addRecipient = function() {
             $scope.recipients.push($scope.recipient);
             $scope.recipient = '';
-        }
+        };
 
         $scope.removeRecipient = function(index) {
             $scope.recipients.splice(index, 1);
-        }
+        };
 
-
-        $scope.generateID = function(L) {
-            var s = '';
-            var randomchar = function() {
-                var n = Math.floor(Math.random() * 62);
-                if (n < 10) return n; //1-10
-                if (n < 36) return String.fromCharCode(n + 55); //A-Z
-                return String.fromCharCode(n + 61); //a-z
-            }
-            while (s.length < L) s += randomchar();
-            $scope.sendingID = s;
-        }
+        $scope.getEmailTemplates = function() {
+            m.templates.list(params, function(data) {
+                console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    $scope.emailTemplates.push(data[i].name);
+                }
+                $scope.emailTemplate = data[0].name;
+            }, function(error) {
+                console.log(error);
+            });
+        };
 
         $scope.previewEmail = function() {
 
+            //Clear modal content
+            $('.modal-body').empty();
+
             var params = {
-                "template_name": "test",
-                "template_content": [{
-                    "name": "main",
-                    "content": $scope.mailContent
+                'template_name': $scope.emailTemplate,
+                'template_content': [{
+                    'name': 'title',
+                    'content': $scope.mailSubject,
                 }, {
-                    "name": "signature",
-                    "content": $scope.user.signature
+                    'name': 'main',
+                    'content': $scope.mailContent
                 }, {
-                    "name": "cta",
-                    "content": "<a href='http://localhost:9000/landing?file=" + $scope.sendingID + "'><img style='border:none;' src='http://voeux.iseabloom.com/img/mail/btn-voir.png' width='345' height='58'/></a>"
+                    'name': 'signature',
+                    'content': $scope.user.signature
                 }]
             };
 
-
             //Get Preview Template from Mandrill
-            m.templates.render(params, function(object) {
-               console.log(object.html);
-
+            m.templates.render(params, function(data) {
+                $('.modal-body').append(data.html);
             }, function(err) {
-                onErrorLog(err);
+                console.log(err);
             });
 
-        }
+        };
 
-
-        $scope.submitForm = function($http) {
+        $scope.submitForm = function() {
 
             //set mandrill params and send email to all recipients
             var params = {
-                "template_name": "test",
-                "template_content": [{
-                    "name": "main",
-                    "content": $scope.mailContent
+                'template_name': $scope.emailTemplate,
+                'template_content': [{
+                    'name': 'title',
+                    'content': $scope.mailSubject,
                 }, {
-                    "name": "signature",
-                    "content": $scope.user.signature
+                    'name': 'main',
+                    'content': $scope.mailContent
                 }, {
-                    "name": "cta",
-                    "content": "<a href='http://localhost:9000/landing?file=" + $scope.sendingID + "'><img style='border:none;' src='http://voeux.iseabloom.com/img/mail/btn-voir.png' width='345' height='58'/></a>"
+                    'name': 'signature',
+                    'content': $scope.user.signature
                 }],
-                "message": {
-                    "subject": $scope.mailSubject,
-                    "from_email": $scope.user.email,
-                    "from_name": $scope.user.firstName + " " + $scope.user.lastName,
-                    "to": $scope.recipients,
-                    "headers": {
-                        "Reply-To": $scope.user.email
+                'message': {
+                    'subject': $scope.mailSubject,
+                    'from_email': $scope.user.email,
+                    'from_name': $scope.user.firstName + ' ' + $scope.user.lastName,
+                    'to': $scope.recipients,
+                    'headers': {
+                        'Reply-To': $scope.user.email
                     },
-                    "tags": [
-                        "iseaTest"
+                    'tags': [
+                        'iseaTest'
                     ],
-                    "track_opens": true,
-                    "track_clicks": true,
+                    'track_opens': true,
+                    'track_clicks': true,
                 }
             };
-            m.messages.sendTemplate(params); // Send Email Template
+
+            mandrill.messages.sendTemplate(params); // Send Email Template
 
             //send a copy to the sender if required
             if ($scope.copyEmail) {
                 params.message.to = []; // Empty recipients array
                 params.message.to = $scope.user; // Add sender mail as recipient
-                m.messages.sendTemplate(params); // Send Email Template
+                mandrill.messages.sendTemplate(params); // Send Email Template
             }
 
-            // Create txt file containing custom e-card message (useful for Flash animation)
-            myServices.createTxtFile($scope.sendingID, $scope.ecardContent).then(function(callback) {
-                alert(callback)
-            });
+        };
 
 
-        }
-
-
-    })
-    .factory('myServices', function($http) {
-        return {
-            createTxtFile: function(sendingID, ecardContent) {
-                //since $http.get returns a promise,
-                //and promise.then() also returns a promise
-                //that resolves to whatever value is returned in it's
-                //callback argument, we can return that.
-                var data = {
-                    sendingID: sendingID,
-                    ecardContent: ecardContent
-                };
-                var postData = $.param(data); // serialize data for sending to php script through POST method
-                return $http({
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    url: '/scripts/php/createTxtFile.php',
-                    data: postData
-                }).then(function(result) {
-                    return result.data;
-                });
-
-            },
-            createEmailPreview: function(html, sendingID) {
-                //since $http.get returns a promise,
-                //and promise.then() also returns a promise
-                //that resolves to whatever value is returned in it's
-                //callback argument, we can return that.
-                var data = {
-                    html: html,
-                    sendingID: sendingID
-                };
-                var postData = $.param(data); // serialize data for sending to php script through POST method
-                return $http({
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    url: '/scripts/php/createEmailPreview.php',
-                    data: postData
-                }).then(function(result) {
-                    return result.data;
-                });
-
-            }
-
-        }
     });
